f9bfb24b4d62d5bedbe58bff81e8fa4dd1fb3b1a
Dagurechaff <box153m@ya.ru>
2025-02-06 23:06:59 +0300

    #19

---
+++ 19.py
@@ -0,0 +1,14 @@
+def f(s,p):
+    if s<=73 and p==3:
+        return True
+    if s <= 73 and p == 2:
+        return False
+    if s > 73 and p == 3:
+        return False
+    if p%2==0:
+        return f(s-2, p+1) or f(s-4, p+1) or f(s//2, p+1)
+    else:
+        return f(s - 2, p + 1) and f(s - 4, p + 1) and f(s // 2, p + 1)
+for s in range(1000,73,-1):
+    if f(s,1):
+        print(s)
